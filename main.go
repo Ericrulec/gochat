966873eada2743dbe79b97f717091059003de4a8
Erik Jensson <erik@jensson.no>
2023-11-22 13:22:57 +0100

    prototyping

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-    "github.com/Ericrulec/gochat/proto"
-    "github.com/Ericrulec/gochat/db"
+	"context"
+	"log"
+	"net"
+
+    //"github.com/Ericrulec/gochat/db"
+	"github.com/Ericrulec/gochat/proto"
 
 	"google.golang.org/grpc"
 
@@ -11,4 +15,38 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func main(){}
+const (
+    PORT = "3000"
+)
+
+type Server struct {
+	proto.UnimplementedMessagingServiceServer
+}
+
+
+func (s *Server) GetUser(ctx context.Context, in *proto.QueryUser) (*proto.User, error) {
+    if in.UserId == "" {
+        return nil, status.Error(
+            codes.InvalidArgument, "UserId must be non-empty",
+            )
+    }
+	return &proto.User{
+        UserId: "1",
+        Name: "Erik Jensson",
+        Status: proto.User_ONLINE,
+	}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		log.Fatalln("Failed to create listener:", err)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+
+	proto.RegisterMessagingServiceServer(s, &Server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
+}
